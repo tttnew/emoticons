25a49246f8f13fd7e556efb5e6022d4b8e0d926d
tttnew <radmil.runet@gmail.com>
2017-10-10 03:38:27 +0500

    remove warnings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Game from "./components/Game";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import "../css/GameScreen.css";
 
 const GameScreen = ({ children }) => (
